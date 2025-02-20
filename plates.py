6b25ae455844bfbf79b83a67881c7a8aa8903dd9
stevecrawshaw <murrayjarvis@gmail.com>
2025-02-20 20:46:32 +0000

    plates ok

--- plates.py
+++ plates.py
@@ -8,17 +8,11 @@ def main():
 
 
 # %%
-def findpos(teststr, minmax="min"):
-    numpos = []
-    for i, x in enumerate(teststr):
-        if x.isnumeric():
-            numpos.append(i)
-    if minmax == "min":
-        return min(numpos)
-    elif minmax == "max":
-        return max(numpos)
-    else:
-        return None
+def test_not_zero_first(s):
+    for i in s:
+        if i.isnumeric():
+            return i != "0"
+    return True
 
 
 # %%
@@ -26,61 +20,41 @@ def is_valid(s):
     length_ok = 2 <= len(s) <= 6
     alnums_ok = s.isalnum()
     first_2_chars = s[0:2].isalpha()
-    # num_min_ok = findpos(s, "min") > 2
-    if length_ok and not s.isalpha():
-        num_max_ok = (findpos(s, "max") + 1) == len(s)
-        first_num_not_0 = s[findpos(s, "min")] != "0"
+    not_zero_first = test_not_zero_first(s)
+    if length_ok and alnums_ok and first_2_chars and not_zero_first:
+        return midnums(s)
     else:
-        num_max_ok = False
-        first_num_not_0 = False
-    if length_ok and alnums_ok and first_2_chars and num_max_ok and first_num_not_0:
-        return True
-    else:
-        return {
-            "length_ok": length_ok,
-            "alnums_ok": alnums_ok,
-            "first_2_chars": first_2_chars,
-            "num_max_ok": num_max_ok,
-            "first_num_not_o": first_num_not_0,
-        }
+        return False
 
 
 # %%
 
 
+# %%
+def midnums(s):
+    """ "
+    test if the chars after position 2 don't have numbers to the left of letters
+    """
+
+    def make_tup_alpha(s):
+        s_2 = s[2:]
+        return tuple([int(x.isalpha()) for x in s_2])
+
+    stringbool = make_tup_alpha(s)
+    match stringbool:
+        case (0, 0, 1, 0) | (0, 0, 0, 1) | (0, 1, 1, 1) | (0, 1, 1, 0):
+            return False
+        case (0, 0, 1) | (0, 1, 0) | (0, 1, 1):
+            return False
+        case (0, 1):
+            return False
+        case _:
+            return True
+
+
+# %%
+# %%
 if __name__ == "__main__":
     main()
 # %%
-
-for i, x in enumerate("CS50P2"):
-    print(i, x)
-
 # %%
-s = "CS50P2"
-if s[-1].isnumeric() and s[3:-2].isalpha():
-    print("bad")
-
-# %%
-"CS50P2"[2:-1]
-
-
-# %%
-def test_str(s):
-    ls = []
-    for x in s[2:]:
-        ls.append(x.isnumeric())
-    return ls
-
-
-# %%
-
-if test_str("CS50P2")[-1]:
-    if any(test_str("CS50P2")[2:-1]):
-        print("bad")
-
-
-# %%
-test_str("CS50P2")[-1]
-
-# %%
-test_str(("CS50P2")[2:-1])
