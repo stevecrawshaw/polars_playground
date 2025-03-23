df81d2c866062a3f3daca5c679174b53d81226b5
stevecrawshaw <murrayjarvis@gmail.com>
2025-03-23 10:16:26 +0000

    sync

--- httpx-test.py
+++ httpx-test.py
@@ -1,5 +1,5 @@
 # %%
-import asyncio
+# import asyncio
 import zipfile
 from pathlib import Path
 
@@ -16,12 +16,12 @@ urls = [
     for m in mo
     if not (y == 2024 and m > 4)
 ]
+print(urls)
 # %%
 con = ibis.connect("duckdb://")
 
+
 # %%
-
-
 def filt(ibtbl: ibis.table):
     return (
         ibtbl.filter(ibtbl.lat.between(51.3, 51.6), ibtbl.lon.between(-2.9, -2.2))
@@ -51,49 +51,71 @@ def filt(ibtbl: ibis.table):
 
 
 # %%
-async def fetch_data(url):
+# async def fetch_data(url):
+#     fname = Path(url).name
+#     fullpath = Path("/tmp") / fname
+#     csvpath = Path("/tmp") / fname.replace(".zip", ".csv")
+#     async with httpx.AsyncClient() as client:
+#         response = await client.get(url)
+#         with open(fullpath, "wb") as f:
+#             f.write(response.content)
+#         with zipfile.ZipFile(fullpath, "r") as zip_ref:
+#             zip_ref.extractall("/tmp")
+#         con.read_csv(
+#             csvpath,
+#             header=True,
+#             types={"P1": "DOUBLE", "P2": "DOUBLE"},
+#             ignore_errors=True,
+#         ).pipe(filt).to_parquet(f"/tmp/{fname.replace('.zip', '.parquet')}")
+
+#         print(f"Downloaded {fname} to {fullpath}")
+#         fullpath.unlink()
+#         csvpath.unlink()
+#         return response.status_code
+
+# %%
+
+
+def fetch_data_httpx(url):
     fname = Path(url).name
     fullpath = Path("/tmp") / fname
     csvpath = Path("/tmp") / fname.replace(".zip", ".csv")
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        with open(fullpath, "wb") as f:
-            f.write(response.content)
-        with zipfile.ZipFile(fullpath, "r") as zip_ref:
-            zip_ref.extractall("/tmp")
-        con.read_csv(
-            csvpath,
-            header=True,
-            types={"P1": "DOUBLE", "P2": "DOUBLE"},
-            ignore_errors=True,
-        ).pipe(filt).to_parquet(f"/tmp/{fname.replace('.zip', '.parquet')}")
+    response = httpx.get(url)
+    with open(fullpath, "wb") as f:
+        f.write(response.content)
+    with zipfile.ZipFile(fullpath, "r") as zip_ref:
+        zip_ref.extractall("/tmp")
+    con.read_csv(
+        csvpath,
+        header=True,
+        types={"P1": "DOUBLE", "P2": "DOUBLE"},
+        ignore_errors=True,
+    ).pipe(filt).to_parquet(f"/tmp/{fname.replace('.zip', '.parquet')}")
 
-        print(f"Downloaded {fname} to {fullpath}")
-        fullpath.unlink()
-        csvpath.unlink()
-        return response.status_code
-
-
-# %%
-
-
-async def main():
-    # urls = [
-    #     "https://archive.sensor.community/csv_per_month/2023-01/2023-01_ds18b20.zip",
-    #     "https://archive.sensor.community/csv_per_month/2023-02/2023-02_ds18b20.zip",
-    #     "https://archive.sensor.community/csv_per_month/2023-03/2023-03_ds18b20.zip",
-    # ]
-    tasks = [fetch_data(url) for url in urls]
-    responses = await asyncio.gather(*tasks)
-
-    for res in responses:
-        print(res)
+    print(f"Downloaded {fname} to {fullpath}")
+    fullpath.unlink()
+    csvpath.unlink()
+    return response.status_code
 
 
 # %%
 
-asyncio.run(main())
 
+# async def main():
+#     tasks = [fetch_data(url) for url in urls]
+#     responses = await asyncio.gather(*tasks)
+
+
+#     for res in responses:
+#         print(res)
+def main():
+    [fetch_data_httpx(url) for url in urls]
+
+
+# %%
+if __name__ == "__main__":
+    # asyncio.run(main())
+    main()
 # %%
 # ibis.read_parquet("/tmp/2023-01_ds18b20.parquet").to_polars().glimpse()
 
